90224debb7c45c3ea23446da9116f995293962bd
Nailan Ferreira <nailanferreira4052@gmail.com>
2025-08-09 18:49:01 -0300

    Resolução do problema 1008

---
+++ Python/1008_salario.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+numero = int(input())
+total_horas = int(input())
+valor_horas = float(input())
+
+print(f"NUMBER = {numero}")
+print(f"SALARY = U$ {total_horas * valor_horas:.2f}")
