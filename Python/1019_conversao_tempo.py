d6d513f4f6683fec31fbf282a13369436f823151
Nailan Ferreira <nailanferreira4052@gmail.com>
2025-09-01 19:43:08 -0300

    Resolução do problema 1019 v2

--- Python/1019_conversao_tempo.py
+++ Python/1019_conversao_tempo.py
@@ -2,10 +2,8 @@
 
 qtd_segundos = int(input())
 
-horas = qtd_segundos // 3600
-qtd_segundos = qtd_segundos-horas*3600
-minutos = qtd_segundos // 60
-qtd_segundos = qtd_segundos-minutos*60
+horas, qtd_segundos = divmod(qtd_segundos, 3600)
+minutos, qtd_segundos = divmod(qtd_segundos, 60)
 segundos = qtd_segundos
 
 print(f"{horas}:{minutos}:{segundos}")
